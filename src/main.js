dd478360f08dd55701b601fd7c397d49fcdc537d
codekyz <codekyz0111@gmail.com>
2022-02-09 06:40:44 +0900

    Revert ":bug: Fix publish error"
    This reverts commit 38964d7a77c03d89c5df34d4aa61ef57748a4bf8.

--- src/main.js
+++ src/main.js
@@ -2,9 +2,21 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/index';
 import store from './store/index';
+import { initializeApp } from 'firebase/app';
 
+const firebaseConfig = {
+    apiKey: "AIzaSyBn1sRkOroH7wObiWWDpDmkW86WcYI4oYs",
+    authDomain: "percent-f0f7e.firebaseapp.com",
+    projectId: "percent-f0f7e",
+    storageBucket: "percent-f0f7e.appspot.com",
+    messagingSenderId: "1034086828313",
+    appId: "1:1034086828313:web:20b34354c443560c03dd88"
+};
+
+const app = initializeApp(firebaseConfig);
 
 createApp(App)
     .use(router)
     .use(store)
+    .use(app)
     .mount('#app')
